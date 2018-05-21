dfd0c9576e802272478452f6aa05c183ad9dbe43
MasterUNG <phrombutr@gmail.com>
2018-05-22 04:15:21 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+
+  colorH1 = '#1313ff';
+
+  onClickNormal() {
+    console.log('You Just Click Show on Console');
+  }
+
+  onClickDouble() {
+    console.log('You Double Click');
+    window.alert('Double Click');
+  }
+
+  onMouseOver() {
+    console.log('You Mouse Over');
+    this.colorH1 = '#ff2621';
+  }
+
+  onMouseOut() {
+    this.colorH1 = '#1313ff';
+  }
+
+} // AppComponent
